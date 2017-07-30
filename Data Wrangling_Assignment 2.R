bc565d9cc56b37f45ef086158ec8d14269eacac7
Arun Bharadwaj <arunbharadwaj2009@gmail.com>
2017-07-30 11:21:26 -0700

    Commit after completing first 2 steps mentioned in Springboard

--- Data Wrangling_Assignment 2.R
+++ Data Wrangling_Assignment 2.R
@@ -1,5 +1,7 @@
 library(dplyr)
 library(tidyr)
+library(ggplot2)
+
 titanic <- titanic3
 summary(titanic)
 
@@ -7,8 +9,22 @@ summary(titanic)
 titanic$embarked[titanic$embarked == ""] <- NA
 
 # Identify rows where embarked is NA
-embarked_missingrows <- which(is.na(titanic$embarked))
+embarked_missingvalues <- which(is.na(titanic$embarked))
 
 # Convert missing rows into S
-titanic$embarked[embarked_missingrows] <- "S"
+titanic$embarked[embarked_missingvalues] <- "S"
+
+# Identify missing values in Age column
+age_missingvalues <- which(is.na(titanic$age))
+
+# Find mean of age column`s non-missing values
+age_mean <- mean(titanic$age,na.rm = TRUE)
+
+# Use mean of age to populate missing values
+titanic$age[age_missingvalues] <- age_mean
+
+# Plot a histogram of original dataset`s age column to think about the other ways to populate missing values
+hist(titanic3$age)
+
+
 
