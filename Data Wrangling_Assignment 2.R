708fa7b6cee8760be4b9c499ac11d89c11f879a3
Arun Bharadwaj <arunbharadwaj2009@gmail.com>
2017-07-30 10:39:38 -0700

    Next commit

--- Data Wrangling_Assignment 2.R
+++ Data Wrangling_Assignment 2.R
@@ -2,4 +2,13 @@ library(dplyr)
 library(tidyr)
 titanic <- titanic3
 summary(titanic)
-which(is.na(titanic$embarked))
+
+# Convert empty string embarked rows into NA
+titanic$embarked[titanic$embarked == ""] <- NA
+
+# Identify rows where embarked is NA
+embarked_missingrows <- which(is.na(titanic$embarked))
+
+# Convert missing rows into S
+titanic$embarked[embarked_missingrows] <- "S"
+
