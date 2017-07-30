868c95a8c3ba1b0759860ca1c74250ce549ded11
Arun Bharadwaj <arunbharadwaj2009@gmail.com>
2017-07-30 15:18:54 -0700

    Final commit

--- Data Wrangling_Assignment 2.R
+++ Data Wrangling_Assignment 2.R
@@ -36,7 +36,7 @@ Summarise_sex_OWTPAC <- titanic3 %>% group_by(sex) %>% summarise(a = mean(age,na
 
 # Summarise the mean of original dataset`s age column based on pclass to see if there are major differences between the means of 1,2 and 3 pclass
 Summarise_pclass_OWTPAC <- titanic3 %>% group_by(ï..pclass) %>% summarise(b = mean(age,na.rm=TRUE))
-Summarise_pclass_OWTPAC
+
 # Grouping dataset based on pclass and finding means shows that pclass = 1 have mean of 39.15, pclass =2 have mean 29.5 and pclass = 3 have mean 24.8
 
 #countNA_pclass <- titanic3  %>% group_by(ï..pclass) %>% summarise((c=sum(is.na(titanic3$age)))) 
@@ -54,3 +54,26 @@ countNA_pclass3 <- sum(is.na(titanic3$age[601:1309]))
 # After calculating number of NA`s in respective pclass, we can find the sum product of number of NA`s in each pclass and mean age of each pclass. If this sum product is further divided by the total number of NA`s, the result turns out to be 27.22, slightly lower than the mean of the whole dataset. This difference is due to the larger proportion of NA`s in pclass =3, which has a lower mean than the whole dataset.
 # } This ends the discussion about other ways to populate age columns with NA values. While the above method of grouping based on pclass and assigning 3 different means for the 3 types of NA`s maybe more accurate, the overall difference is not very significant.
 
+# Convert empty string boat rows into NA
+titanic$boat[titanic$boat == ""] <- NA
+
+# Duplicate cabin column to new column named has_cabin_number
+titanic$has_cabin_number <- titanic$cabin
+
+# Identify rows where has_cabin_number column does not have NA values
+cabin_number_notNA <- (!is.na(titanic$has_cabin_number))
+
+# Convert these not NA values in has_cabin_number to 1
+titanic$has_cabin_number[cabin_number_notNA] <- 1
+
+# Identify rows where has_cabin_number column has NA values
+cabin_number_NA <- (is.na(titanic$has_cabin_number))
+
+# Convert these NA values in has_cabin_number to 0
+titanic$has_cabin_number[cabin_number_NA] <- 0
+
+# Save new file as titanic_clean
+titanic_clean <- titanic
+
+# Write final csv output file to working directory
+write.csv(titanic_clean,file="titanic_clean.csv")
